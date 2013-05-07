98d1035fe018a6864d5cef56945c0d6b88fddd40
gatespace <gatespace.k@gmail.com>
2013-05-07 20:15:15 +0900

    Revert "test"
    This reverts commit 02bf4c49bde98809f2cc20439483a693970e927e.

--- comments.php
+++ comments.php
@@ -9,14 +9,16 @@
  *
  * @package _s
  */
+?>
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
-if ( post_password_required() )
-	return;
+<?php
+	/*
+	 * If the current post is protected by a password and
+	 * the visitor has not yet entered the password we will
+	 * return early without loading the comments.
+	 */
+	if ( post_password_required() )
+		return;
 ?>
 
 	<div id="comments" class="comments-area">
